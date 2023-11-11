7140f988f07a768e8e024e9ce47fc43f0e8cdad9
Matheus Nogueira <95196338+MatheusNogueiraUfv@users.noreply.github.com>
2023-11-10 21:20:11 -0300

    Ultimas modificações

--- C/Proprios/Trabalhos_PAA/TP2/main.c
+++ C/Proprios/Trabalhos_PAA/TP2/main.c
@@ -6,18 +6,6 @@ int main(){
     PilhaCoordenadas PtrCoordenadas;
     Fila filas;
     Aventureiro ptrAv;
-    //printf("PASSOU!\n");
-    for(int i = 0;i<ptr.LinhasMapa;i++){
-        for(int j = 0;j<ptr.ColunasMapa;j++){
-            //printf(" [%d][%d]=>[%d] ",i,j,ptr.ConteudoMapa[i][j]);
-        }
-        //printf("\n");
-    }
-    //printf("HP: %d\n",ptr.VidaJogador);
-    //printf("Linha e Coluna Inicial: %d %d \n ",ptr.LinhasInicial,ptr.ColunasInicial);
-    //printf("Linha e Coluna Final: %d %d \n ",ptr.LinhasFinal,ptr.ColunasFinal);
     IniciarDeslocamento(&ptr,&ptrAv,&PtrCoordenadas,&filas);
-    //ApresentarTabelaSimplex(&ptrAv,&ptr);
-
     return 0;
 }
